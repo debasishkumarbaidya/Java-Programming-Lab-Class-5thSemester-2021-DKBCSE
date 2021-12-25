ece999e1a0ebd353021d92e94dadae5eb2708afb
Debasish Kumar Baidya <85603537+debasishkumarbaidya@users.noreply.github.com>
2021-12-25 20:25:35 +0530

    Add files via upload

--- MainShape.java
+++ MainShape.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import Shape.*;
 class MainShape
 {
     public static void main(String args[]) throws IOException
